25fecb48f2599dcf6c23b68860c621b01134a3fe
BUBBEL <BUBBEL@LAPTOP-HMKDTUGT>
2020-09-05 17:17:50 +0200

    Cleaned

--- stack/src/stack.c
+++ stack/src/stack.c
@@ -1,13 +1,32 @@
+/*
+ *	stack.c
+ *
+ *	Creates a stack of size 7 that can be filled with double variables.
+ *	The push and pop functions add and remove variables from the stack.
+ *	Strings can not be added and chars will not work correctly.
+ *	The double minimum value is reserved and should not be used.
+ *
+ *	Written by Jakob Östberg on 05/09/2020
+ *
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <limits.h>
 
 #define STACK_LENGTH 7
-#define EMPTY_STACK INT_MIN
+#define DOUBLE_MIN -1.7976931348623157E+308
+#define EMPTY_STACK DOUBLE_MIN
 
 double theStack[STACK_LENGTH];
 int top = -1;
 
+/*
+ * 	push()
+ *
+ *	Pushes one double value to the stack.
+ *	input: double
+ *	output: TRUE if success, FALSE if failure
+ */
 bool push(double pushValue) {
 	if (top >= STACK_LENGTH - 1) {
 		printf("ERROR: Can't push to full stack.\n");
@@ -19,6 +38,13 @@ bool push(double pushValue) {
 	return true;
 }
 
+/*
+ * 	pop()
+ *
+ *	Pops a double variable from the stack
+ *	input: void
+ *	output: double if successful, EMPTY_STACK if stack is empty
+ */
 double pop(void) {
 	if (top == -1) {
 		printf("ERROR: Can't pop from empty stack.\n");
--- stack/src/test_stack.c
+++ stack/src/test_stack.c
@@ -1,81 +1,85 @@
+/*
+ *	test_stack.c
+ *
+ *	Creates a series of unit tests that tests the functionality
+ *	of the push() and pop() functions in stack.c
+ *
+ *	Written by Jakob Östberg on 05/09/2020
+ *
+ */
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
 #include "stack.h"
 
-double pushPop(double);
-
 struct Test {
-   char  desc[50];
-   double testValue;
-   double expectedReturnValue;
-   double returnValue;
+	char desc[50];
+	double testValue;
+	double expectedReturnValue;
+	double returnValue;
 };
 
-void runTest( struct Test );
+bool runTest(struct Test);
 
-char desc[5][40] =
-{ "int",
-  "decimal",
-  "negative",
-  "char",
-  "zero"
-};
+int main() {
 
-double testValues[5] = {11, 9000.0084, -42.123456, 'd', 0};
-double expectedReturnValues[5] = {11.89, 9000.0084, -42.123456, 100, 0};
-double returnValues[5];
+	// DECLARE TESTS
+	struct Test test1;
+	struct Test test2;
+	struct Test test3;
+	struct Test test4;
+	struct Test test5;
 
+	// Test 1 - int
+	strcpy(test1.desc, "int");
+	test1.testValue = 11;
+	test1.expectedReturnValue = 11;
 
-int main () {
+	// Test 2 - decimal
+	strcpy(test2.desc, "decimal");
+	test2.testValue = 9000.0084;
+	test2.expectedReturnValue = 9000.0084;
 
+	// Test 3 - negative
+	strcpy(test3.desc, "negative");
+	test3.testValue = -42.123456;
+	test3.expectedReturnValue = -42.123456;
 
-// DECLARE TESTS
-   struct Test test1;
-   struct Test test2;
-   struct Test test3;
-   struct Test test4;
-   struct Test test5;
+	// Test 4 - char
+	strcpy(test4.desc, "char");
+	test4.testValue = 'd';
+	test4.expectedReturnValue = 100;
 
-   strcpy( test1.desc, "int");
-   strcpy( test2.desc, "decimal");
-   strcpy( test3.desc, "negative");
-   strcpy( test4.desc, "char");
-   strcpy( test5.desc, "zero");
-   test1.testValue = 11;
-   test2.testValue = 9000.0084;
-   test3.testValue = -42.123456;
-   test4.testValue = 'd';
-   test5.testValue = 0;
-   test1.expectedReturnValue = 11;
-   test2.expectedReturnValue = 9000.0084;
-   test3.expectedReturnValue = -42.123456;
-   test4.expectedReturnValue = 100;
-   test5.expectedReturnValue = 0;
-   
-// RUN TESTS
+	// test 5 - zero
+	strcpy(test5.desc, "zero");
+	test5.testValue = 0;
+	test5.expectedReturnValue = 0;
+
+	// RUN TESTS
 	runTest(test1);
 	runTest(test2);
 	runTest(test3);
 	runTest(test4);
 	runTest(test5);
-	
-   return 0;
+
+	return 0;
 }
-
-void runTest( struct Test test ){
-
+/*
+ * 	runTest()
+ *
+ *	Tests if a pushed variable is the same as the popped variable
+ *	input: struct
+ *	output: TRUE if success, FALSE if failure
+ */
+bool runTest(struct Test test) {
 	push(test.testValue);
 	test.returnValue = pop();
-	if(test.expectedReturnValue == test.returnValue)
-	{
+	if (test.expectedReturnValue == test.returnValue) {
 		printf("PASS :: %s test was successful\n", test.desc);
-	}else{
-		printf("FAIL :: %s test failed\n", test.desc);
+		return true;
+	} else {
+		printf("FAIL :: %s test failed, expected value was %f but got %f \n",
+				test.desc, test.expectedReturnValue, test.returnValue);
+		return false;
 	}
-}
-
-double pushPop(double pushValue){
-	push(pushValue);
-	return pop();
 }
